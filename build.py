fbc0337ef0c94860f375a443b438907dd5d8da08
kyungjejo <kyungjejo@gmail.com>
2018-08-04 15:51:44 +0900

    semantic ui icon error

--- build.py
+++ build.py
@@ -33,7 +33,7 @@ with open('./backend/build/static/'+css) as text:
     url = re.findall("url\((/static/media.*?)\)", new_css)
     for idx,u in enumerate(url):
         _u = u.strip("/static/")
-        _u = "{% static "+_u+" %}"
+        _u = "static/css/"+_u
         new_css = re.sub(u, _u, new_css)
 with open('./backend/build/static/'+css, 'w') as text:
-    text.write("{% load static %}"+new_css)
+    text.write(new_css)
